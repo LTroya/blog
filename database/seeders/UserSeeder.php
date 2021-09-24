327398316e20af39f6821d32aca28e3f53889a8f
derwin <derwin9227@gmail.com>
2021-09-24 08:36:11 -0500

    creando migraciones y seeders para llenar con registros falsos

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Derwin Hernandez',
+            'email' => 'derwin9227@hotmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+        User::factory(99)->create();
+    }
+}
