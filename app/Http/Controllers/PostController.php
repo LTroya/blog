38312f89c5199bae05a44775c47364ac802747a6
derwin <derwin9227@gmail.com>
2021-09-30 09:48:59 -0500

    habilitar el mostrar detalle de cada post con enlaces a post de la misma categoria, volviendo responsiva la vista del detalle del post. -> linea 27 en comentario manera correcta de mostrar categoria -- metodo utilizado array por errores al momento de ejecutar relacion--

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
-
 
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::where('status', 2)->orderBy('id', 'desc')->paginate(8);
+        $posts = Post::where('status', 2)->latest('id')->paginate(8);
 
         return view('posts.index', compact('posts'));
-    }
+    }//index
+
+    public function show(Post $post, Category $category){
+
+        $similares = Post::where('category_id', $post->category_id)
+        ->where('status', 2)
+        ->where('id','!=', $post->id)
+        ->latest('id')
+        ->take(4)
+        ->get();
+
+        $category = Category::where('id', $post->category_id)->get();
+        
+        return view('posts.show', compact('post', 'category', 'similares'));
+    }//show
 }
